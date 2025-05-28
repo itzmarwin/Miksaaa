954d9e8316aa0d9e749cfe0b87985d43002141f9
itzmarwin <itzm4rw1n@gmail.com>
2025-05-28 21:50:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "http://github.com/Herokutaccc/acx",
+    "https://github.com/itzmarwin/Miksaaa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
