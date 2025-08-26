1efbf2d34545a02c26c4120ea5d4df87ed9d5bc4
itzmarwin <itzm4rw1n@gmail.com>
2025-08-27 01:26:17 +0530

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -14,59 +14,53 @@ from AnonXMusic.utils import monitor
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-
 async def init():
-    # Check assistant client variables
-    if not any([config.STRING1, config.STRING2, config.STRING3, config.STRING4, config.STRING5]):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
-        exit()
+if (
+not config.STRING1
+and not config.STRING2
+and not config.STRING3
+and not config.STRING4
+and not config.STRING5
+):
+LOGGER(name).error("Assistant client variables not defined, exiting...")
+exit()
+await sudo()
+try:
+users = await get_gbanned()
+for user_id in users:
+BANNED_USERS.add(user_id)
+users = await get_banned_users()
+for user_id in users:
+BANNED_USERS.add(user_id)
+except:
+pass
+await app.start()
+for all_module in ALL_MODULES:
+importlib.import_module("AnonXMusic.plugins" + all_module)
+LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
+await userbot.start()
+await Anony.start()
+try:
+await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+except NoActiveGroupCall:
+LOGGER("AnonXMusic").error(
+"Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+)
+exit()
+except:
+pass
+await Anony.decorators()
+LOGGER("AnonXMusic").info(
+"\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
+)
+await monitor()
+await idle()
+await app.stop()
+await userbot.stop()
+LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
 
-    # Load sudo users
-    await sudo()
+if name == "main":
+with warnings.catch_warnings():
+warnings.filterwarnings("ignore", message="There is no current event loop", category=DeprecationWarning)
+asyncio.get_event_loop().run_until_complete(init())
 
-    # Load banned users
-    try:
-        users = await get_gbanned()
-        for user_id in users:
-            BANNED_USERS.add(user_id)
-
-        users = await get_banned_users()
-        for user_id in users:
-            BANNED_USERS.add(user_id)
-    except Exception:
-        pass
-
-    # Start bot & userbot
-    await app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("AnonXMusic.plugins." + all_module)
-    LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
-
-    await userbot.start()
-    await Anony.start()
-
-    # Decorators load
-    await Anony.decorators()
-
-    LOGGER("AnonXMusic").info(
-        "AnonX Music Bot Started Successfully.\n\nDon't forget to visit @FallenAssociation"
-    )
-
-    # Start monitor & idle loop
-    await monitor()
-    await idle()
-
-    # Stop clients on exit
-    await app.stop()
-    await userbot.stop()
-    LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
-
-
-if __name__ == "__main__":
-    with warnings.catch_warnings():
-        warnings.filterwarnings(
-            "ignore",
-            message="There is no current event loop",
-            category=DeprecationWarning,
-        )
-        asyncio.run(init())
